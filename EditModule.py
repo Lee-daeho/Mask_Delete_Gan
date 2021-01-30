b49e49d3ac1ad7bc8c9c376bc1f206c2ccf9c788
scilab <zmnh2003@gmail.com>
2021-01-30 23:23:58 +0900

    mask end

--- EditModule.py
+++ EditModule.py
@@ -14,12 +14,17 @@ class editEncoder(nn.Module):
     def __init__(self):
         super(editEncoder, self).__init__()
 
-        self.conv1 = self.doubleConv(4,16)      #downsample
-        self.conv2 = self.doubleConv(16,32)
-        self.conv3 = self.doubleConv(32,64)
+        self.conv1 = self.doubleConv(4,16)      #downsample     #ADD SE block
+        self.SE1 = self.SENetwork(16, 16)
+        self.conv2 = self.doubleConv(16,32)         #ADD SE block
+        self.SE2 = self.SENetwork(32, 16)
+        self.conv3 = self.doubleConv(32,64)         #ADD SE block
+        self.SE3 = self.SENetwork(64, 16)
         self.conv4 = self.doubleConv(64,128)
 
         self.middle_conv = self.doubleConv(128,256) #middle
+
+        #ADD ATROUS CONVOLUTION BLOCK --> RATE : 2,4,8,16
 
         self.deconv1 = self.deconv(256,128) #upsample
         self.deconv2 = self.deconv(128,64)
@@ -37,3 +42,41 @@ class editEncoder(nn.Module):
         self.upconv4 = self.doubleConv(32, 16)
 
         self.pool = nn.MaxPool2d(2, stride=2)
+
+    def conv(self, in_channel, out_channel):
+        convolution = nn.Sequential(
+            nn.Conv2d(in_channel, out_channel, kernel_size=3,stride=1, padding=1),
+            nn.InstanceNorm2d(out_channel),
+            nn.LeakyReLU()
+        )
+
+        return convolution
+
+    def deconv(self, in_channel, out_channel):
+        deconvolution = nn.Sequential(
+            nn.ConvTranspose2d(in_channel, out_channel, kernel_size=2, stride=2, padding=0),
+            nn.InstanceNorm2d(out_channel),
+            nn.LeakyReLU()
+        )
+
+        return deconvolution
+
+    def doubleConv(self, in_channel, out_channel):
+        dConv = nn.Sequential(
+            self.conv(in_channel, out_channel),
+            self.conv(out_channel, out_channel)
+        )
+
+        return dConv
+
+    def SENetwork(self, in_channel, ratio):
+
+        SENet = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1,1)),
+            nn.Linear(in_channel, in_channel/ratio),
+            nn.ReLU(),
+            nn.Linear(in_channel/ratio, in_channel),
+            nn.Sigmoid()
+        )
+
+        return SENet
